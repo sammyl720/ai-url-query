7e1fdaeab138098faacef97d05fbdb02bb78e341
sam <sam.leider@hotmail.com>
2025-02-02 00:44:23 -0500

    move example to example/cli.ts

---
+++ src/helpers.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Determine current directory since ES modules do not have __dirname.
+const __filename = fileURLToPath(import.meta.url);
+
+/** Absolute path to `src` or `dist` when running built package. */
+export const __dirname = path.dirname(__filename);
+
+// Path to store the vectra index locally; you can change as needed.
+export const INDEX_PATH = path.join(path.dirname(__dirname), "vectra_index");
